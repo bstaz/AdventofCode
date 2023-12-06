4bced8f154c8cebc59e076e7fc8de7cd4a1f53aa
Brian Staszewski <bstaz@users.noreply.github.com>
2023-12-05 20:14:46 -0800

    More formatting

--- template/solution.py
+++ template/solution.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python
 import rich_click as click
-
 from rich import print
 
 
 @click.command(help="Run the solution for a part: 1|2")
-@click.argument('index', type=int)
-@click.option('--debug', '-d', is_flag=True, default=False, help="Ouput debugging info")
+@click.argument("index", type=int)
+@click.option("--debug", "-d", is_flag=True, default=False, help="Ouput debugging info")
 def main(index: int, debug: bool):
     if debug:
         print("Debugging enabled; setting up Rich traceback handler...")
         from rich.traceback import install
+
         install(show_locals=True)
 
     answer = None
 
     if index == 1:
         print(">>> Solving for part 1")
-        #implement part one here
+        # implement part one here
         pass
 
     elif index == 2:
         print(">>> Solving for part 2")
-        #implement part two here
+        # implement part two here
         pass
 
     else:
