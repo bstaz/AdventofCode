73a75daa80210d615e41855841b7b715b31054f2
Brian Staszewski <bstaz@users.noreply.github.com>
2023-12-05 20:13:19 -0800

    Black formatting

--- 2023/1/solution.py
+++ 2023/1/solution.py
@@ -6,70 +6,77 @@ from rich import print
 
 
 @click.command(help="Run the solution for a part: 1|2")
-@click.argument('index', type=int)
-@click.option('--debug', '-d', is_flag=True, default=False, help="Ouput debugging info")
+@click.argument("index", type=int)
+@click.option("--debug", "-d", is_flag=True, default=False, help="Ouput debugging info")
 def main(index: int, debug: bool):
     if debug:
         print("Debugging enabled; setting up Rich traceback handler...")
         from rich.traceback import install
+
         install(show_locals=True)
 
-    inputfile = 'input.txt'
+    inputfile = "input.txt"
     total = 0
 
     if index == 1:
         print(">>> Solving for part 1")
-        digit_re = r'([0-9])'
-        with open(inputfile, 'r') as f:
+        digit_re = r"([0-9])"
+        with open(inputfile, "r") as f:
             for line in f:
                 first, last = None, None
                 value = line.strip()
-                if debug: print(value)
+                if debug:
+                    print(value)
                 items = re.findall(digit_re, value)
                 first = items[0]
                 last = items[-1]
-                if debug: print(first, last)
                 number = int(first + last)
-                if debug: print(number)
+                if debug:
+                    print(first, last)
+                    print(number)
                 total += number
 
     elif index == 2:
         print(">>> Solving for part 2")
-        number_re = r'(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))'
+        number_re = r"(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))"
         # Lookahead    ^^^
         # This sovles the problem with '4eight5mjlkzrgnmlnmxntqmtlxmqlkjccttcpmgznfouroneightk'
         # where the 'oneight' problem at the end won't get both 'one' and 'eight' otherwise.
         number_values = {
-            'one': 1,
-            'two': 2,
-            'three': 3,
-            'four': 4,
-            'five': 5,
-            'six': 6,
-            'seven': 7,
-            'eight': 8,
-            'nine': 9
+            "one": 1,
+            "two": 2,
+            "three": 3,
+            "four": 4,
+            "five": 5,
+            "six": 6,
+            "seven": 7,
+            "eight": 8,
+            "nine": 9,
         }
-        with open(inputfile, 'r') as f:
+        with open(inputfile, "r") as f:
             for line in f:
                 first, last = None, None
                 value = line.strip()
-                if debug: print(f"{value}:")
+                if debug:
+                    print(f"{value}:")
                 items = re.findall(number_re, value)
-                if debug: print(items)
-                if debug: print(f"  {items[0]}")
+                if debug:
+                    print(items)
+                    print(f"  {items[0]}")
                 try:
                     first = int(items[0])
                 except ValueError:
                     first = number_values[items[0]]
-                if debug: print(f"  {items[-1]}")
+                if debug:
+                    print(f"  {items[-1]}")
                 try:
                     last = int(items[-1])
                 except ValueError:
                     last = number_values[items[-1]]
-                if debug: print(first, last)
                 number = int(str(first) + str(last))
-                if debug: print(f"> {number}")
+                if debug:
+                    print(first, last)
+                    print(f"> {number}")
                 total += number
 
     else:
--- 2023/2/solution.py
+++ 2023/2/solution.py
@@ -4,34 +4,33 @@ from rich import print
 
 
 @click.command(help="Run the solution for a part: 1|2")
-@click.argument('index', type=int)
-@click.option('--debug', '-d', is_flag=True, default=False, help="Ouput debugging info")
+@click.argument("index", type=int)
+@click.option("--debug", "-d", is_flag=True, default=False, help="Ouput debugging info")
 def main(index: int, debug: bool):
     if debug:
         print("Debugging enabled; setting up Rich traceback handler...")
         from rich.traceback import install
+
         install(show_locals=True)
 
-    inputfile = 'input.txt'
+    inputfile = "input.txt"
     answer = 0
 
     if index == 1:
         print(">>> Solving for part 1")
-        maxcubes = {
-            'red': 12,
-            'green': 13,
-            'blue': 14
-        }
-        with open(inputfile, 'r') as f:
+        maxcubes = {"red": 12, "green": 13, "blue": 14}
+        with open(inputfile, "r") as f:
             for line in f:
-                game, data = line.strip().split(': ')
+                game, data = line.strip().split(": ")
                 gameId = game.split()[1]
-                draws = data.split('; ')
-                if debug: print(gameId, draws)
+                draws = data.split("; ")
+                if debug:
+                    print(gameId, draws)
                 gamePossible = True
                 for draw in draws:
-                    for item in draw.split(', '):
-                        if debug: print(item)
+                    for item in draw.split(", "):
+                        if debug:
+                            print(item)
                         count, color = item.split()
                         if int(count) > maxcubes[color]:
                             gamePossible = False
@@ -39,12 +38,13 @@ def main(index: int, debug: bool):
                 if gamePossible:
                     answer += int(gameId)
                 else:
-                    if debug: print(f">>> {gameId} not possible!")
-                    if debug: print(f">>> {line.strip()}")
+                    if debug:
+                        print(f">>> {gameId} not possible!")
+                        print(f">>> {line.strip()}")
 
     elif index == 2:
         print(">>> Solving for part 2")
-        with open(inputfile, 'r') as f:
+        with open(inputfile, "r") as f:
             for line in [x.strip() for x in f]:
                 print(line)
 
