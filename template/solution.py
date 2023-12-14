05d4eeabea54a4c81b3edd7dc1bd86797336de09
Brian Staszewski <bstaz@users.noreply.github.com>
2023-12-14 07:27:18 -0800

    Add Logging

--- template/solution.py
+++ template/solution.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 import os, sys
+import logging
 
 import rich_click as click
 from rich import print
+
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logger = logging.getLogger()
+
+
+class SolutionNotImplementedError(NotImplementedError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+        logger.error(args[0])
 
 
 def parse_input(inputfile: str):
@@ -15,29 +30,32 @@ def parse_input(inputfile: str):
 @click.option("--debug", "-d", is_flag=True, default=False, help="Ouput debugging info")
 def main(index: int, debug: bool):
     if debug:
-        print("Debugging enabled; setting up Rich traceback handler...")
+        logger.setLevel(logging.DEBUG)
+        logger.debug("Debugging enabled")
+        logger.debug("Setting up Rich traceback handler...")
         from rich.traceback import install
         install(show_locals=True)
+        logger.debug(" ...Done.")
 
     inputfile = f"{os.path.dirname(os.path.abspath(sys.argv[0]))}/input.txt"
     gamedata = parse_input(inputfile)
     answer = 0
 
     if index == 1:
-        print(">>> Solving for part 1")
+        logger.info(">>> Solving for part 1")
         # implement part one here
         raise NotImplementedError("Solution 1 is not yet implemented!")
 
     elif index == 2:
-        print(">>> Solving for part 2")
+        logger.info(">>> Solving for part 2")
         # implement part two here
         raise NotImplementedError("Solution 2 is not yet implemented!")
 
     else:
-        print("Invalid index; valid values are 1|2.")
+        logger.error("Invalid index; valid values are 1|2.")
         exit(1)
 
-    print(f"Answer: {answer}")
+    logger.info(f"Answer: {answer}")
 
 
 if __name__ == "__main__":
