d450a329e47758eca01575b06492e4e64bc47728
Brian Staszewski <bstaz@users.noreply.github.com>
2025-12-16 07:56:27 -0800

    Progress bars

--- python/2025/2/solution.py
+++ python/2025/2/solution.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env -S uv run
 
+import time
+
 from rich import print
 from rich.traceback import install as rich_install
 from rich.console import Console
 from rich.table import Table
+from rich.progress import Progress, TextColumn, BarColumn, TaskProgressColumn, TimeRemainingColumn, MofNCompleteColumn
 
 rich_install(show_locals=True)
 
-DEBUG = False
+DEBUG = True
 
 
-def debug_print(text: str, end: str = "\n"):
+def debug_print(console: Console, text: str, end: str = "\n"):
     if not DEBUG:
         return
-    print(text, end=end)
+    console.print(text, end=end)
 
 
 def get_input(path: str):
@@ -36,19 +39,47 @@ def main(input_path: str, expected1: int = 0, expected2: int = 0):
     sum_matches = 0
     sum_matches2 = 0
 
-    for group in input_data:
-        _start, _end = group
-        start = int(_start)
-        end = int(_end)
+    with Progress(
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            MofNCompleteColumn(),
+            TaskProgressColumn(),
+            TimeRemainingColumn(),
+            console=console,
+            transient=True,
+        ) as p:
+        t1 = p.add_task("Groups...", total=len(input_data))
+        for group in input_data:
+            p.update(t1, advance=1)
+            _start, _end = group
+            start = int(_start)
+            end = int(_end)
 
-        for v in range(start, end + 1):
-            _v = str(v)
-            half = int(len(_v) / 2)
-            first, second = (_v[0:half], _v[half:])
-            if first == second:
-                debug_print(f"Match! {_v=} {first=} {second=}")
-                sum_matches += v
+            ranges = range(start, end + 1)
+            with Progress(
+                TextColumn("[progress.description]{task.description}"),
+                BarColumn(),
+                MofNCompleteColumn(),
+                TaskProgressColumn(),
+                TimeRemainingColumn(),
+                console=console,
+                transient=True,
+            ) as p2:
+                t2 = p2.add_task("IDs...", total=len(ranges))
+                p.console.print(group)
+                for v in ranges:
+                    p2.update(t2, advance=1)
+                    #time.sleep(0.0001)
+                    _v = str(v)
+                    half = int(len(_v) / 2)
+                    first, second = (_v[0:half], _v[half:])
+                    if first == second:
+                        debug_print(console, f"\tMatch1: {_v=} {first=} {second=}")
+                        sum_matches += v
 
+
+    if sum_matches != expected1:
+        raise ValueError(f"{sum_matches=} should be {expected1}")
 
     console.print(f"Matches: 1:{sum_matches} 2:{sum_matches + sum_matches2}")
 
@@ -56,7 +87,7 @@ def main(input_path: str, expected1: int = 0, expected2: int = 0):
 if __name__ == "__main__":
     print("=====================================================")
     print("------------------- input.test.txt ------------------")
-    main("input.test.txt")
-    print("-------------------- input.1.txt --------------------")
-    main("input.txt")
+    main("input.test.txt", 1227775554)
+    print("-------------------- input.txt --------------------")
+    main("input.txt", 31839939622)
 
